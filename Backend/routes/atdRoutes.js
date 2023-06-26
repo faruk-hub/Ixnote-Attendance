aad349cd7fc77cdca6191bad16c03181c1d71525
Semmwa <bakshaksemmwa@gmail.com>
2023-06-26 13:43:54 +0100

    fixing mark atd route

--- Backend/routes/atdRoutes.js
+++ Backend/routes/atdRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const atdController = require('../controllers/attendanceController')
 
-router.route('/attendance/:id')
+router.route('/attendance/:student')
       .post(atdController.markAttendance)
 
 
